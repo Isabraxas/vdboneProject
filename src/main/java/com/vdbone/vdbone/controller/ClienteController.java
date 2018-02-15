0b6f293709a4471f5050019899b350046fccc09f
Isabraxas <isvar.vega@gmail.com>
2018-02-15 18:13:50 -0400

    Metodo save agregado sin llave foranea simplificado

--- src/main/java/com/vdbone/vdbone/controller/ClienteController.java
+++ src/main/java/com/vdbone/vdbone/controller/ClienteController.java
@@ -4,13 +4,11 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.vdbone.vdbone.model.Cliente;
 import com.vdbone.vdbone.service.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -21,10 +19,7 @@ public class ClienteController  {
     protected ObjectMapper mapper;
 
     @RequestMapping(value = "/save", method = RequestMethod.POST)
-    public List<Cliente> save(@RequestBody String clienteJson) throws IOException {
-
-        this.mapper= new ObjectMapper();
-        Cliente cliente = this.mapper.readValue(clienteJson, Cliente.class);
+    public List<Cliente> save(@RequestBody Cliente cliente){
 
         this.clienteService.save(cliente);
         return this.clienteService.getAll();
--- src/main/java/com/vdbone/vdbone/dao/ClienteRepository.java
+++ src/main/java/com/vdbone/vdbone/dao/ClienteRepository.java
@@ -4,5 +4,5 @@ import com.vdbone.vdbone.model.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ClienteRepository extends JpaRepository<Cliente,Long> {
-   Cliente save(Cliente cliente);
+  // Cliente save(Cliente cliente);
 }
